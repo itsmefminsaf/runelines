00a79538da37f76dd6b89ab2d9d2ee5ec337b31c
itsmefminsaf <itsmefminsaf@gmail.com>
2025-06-16 11:35:01 +0530

    !feat: making the dbConnect emit only one "connect" event for avoid MaxListenersExceededWarning

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,23 +1,39 @@
-import { error } from "console";
-import mongoose from "mongoose";
+import mongoose, { Mongoose } from "mongoose";
 
 const DB_URI = process.env.DB_URI;
 
-if (!DB_URI) {
-  throw new Error(
-    "Please define the DB_URI environment variable inside .env.local",
-  );
+if (!DB_URI) throw new Error("Please add the DB_URI environmental variable");
+
+let cached: { conn: null | Mongoose; promise: null | Promise<Mongoose> } = (
+  global as unknown as { mongoose: { conn: null; promise: null } }
+).mongoose;
+
+if (!cached) {
+  cached = (
+    global as unknown as {
+      mongoose: { conn: null; promise: null };
+    }
+  ).mongoose = { conn: null, promise: null };
 }
 
-async function dbConnect() {
-  try {
-    mongoose.connection.on("connected", () => {
-      return mongoose.connection.readyState === 1;
-    });
-    await mongoose.connect(DB_URI as string);
-  } catch (err) {
-    error("Failed to connect to MongoDB", err);
+const dbConnect = async () => {
+  if (cached.conn) return cached.conn;
+  if (!cached.promise) {
+    cached.promise = mongoose
+      .connect(DB_URI, {
+        bufferCommands: false,
+      })
+      .then((_mongoose) => {
+        cached.conn = _mongoose;
+        console.log("MongoDB connected");
+        return _mongoose;
+      })
+      .catch((err) => {
+        console.log("Error connecting to mongoDB: ", err);
+        throw err;
+      });
   }
-}
+  return cached.promise;
+};
 
 export default dbConnect;
